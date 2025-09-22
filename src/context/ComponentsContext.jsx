0e1e5a1293cf9601cc71bb3c7910263170fd1421
Remukas <remzyg@gmail.com>
2025-09-22 12:59:54 +0300

    Fix useMemo import error

--- src/context/ComponentsContext.jsx
+++ src/context/ComponentsContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useContext, useEffect, useCallback } from 'react';
+import React, { createContext, useState, useContext, useEffect, useCallback, useMemo } from 'react';
 import { useToast } from '@/components/ui/use-toast';
 
 const ComponentsContext = createContext({
